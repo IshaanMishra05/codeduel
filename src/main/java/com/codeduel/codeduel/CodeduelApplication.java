77c2fb2760947ea5732437b0086746792afe07e1
Ishaan <ishaan.mishra2308@gmail.com>
2026-07-20 11:58:28 +0530

    force rebuild v2

--- src/main/java/com/codeduel/codeduel/CodeduelApplication.java
+++ src/main/java/com/codeduel/codeduel/CodeduelApplication.java
@@ -3,13 +3,10 @@ package com.codeduel.codeduel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// v2 deployment
 @SpringBootApplication
 public class CodeduelApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(CodeduelApplication.class, args);
-
 	}
-
 }
